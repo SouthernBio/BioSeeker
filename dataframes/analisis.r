e7ce7219f9d1a621ab0ecb8e8f7bd4813f4fcb3b
Facundo Martínez <90737264+fx-biocoder@users.noreply.github.com>
2022-01-23 17:16:51 -0300

    Procesamiento de la información
    El dataframe de bicodones ya contiene información sobre codones individuales del par y se le agregó una columna que indica si es un par inhibitorio o no.

--- dataframes/analisis.r
+++ dataframes/analisis.r
@@ -11,6 +11,10 @@ datos_codones$tasaConservacion <- datos_codones$conservacion / datos_codones$ref
 codones_raros <- c("TTT", "TTA", "CTT", "CTC", "CTA", "ATT", "ATA", "GTT", "GTC", "GTA", "TCT", 
                    "TCA", "CCT", "ACT", "ACA", "ACG", "GCT", "GCA", "GCG", "TAT", "CAT", "CAA",
                    "AAT", "AAA", "GAA", "TGT", "CGT", "CGA", "CGG", "GCT", "GGG")
+vector_logico <- c(TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, 
+                   TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, 
+                   TRUE, TRUE, TRUE, TRUE, TRUE)
+dflogic <- data.frame(codones_raros, vector_logico) # Solución momentánea, buscar una forma más elegante
 
 # Establecer codones raros
 # Raro: TRUE, no raro: FALSE
@@ -35,7 +39,7 @@ codones_barplot
 
 # Ahora para bicodones:
 datos_bicodones$tasaConservacion <- datos_bicodones$conservacion / datos_bicodones$referencia
-datos_bicodones$esRaro <- logical(length = 3721)
+datos_bicodones$esRaro <- c(length = 3721) # Default: FALSE
 datos_bicodones$primerCodon <- character(length = 3721)
 datos_bicodones$segundoCodon <- character(length = 3721)
 
@@ -51,8 +55,50 @@ for(i in datos_bicodones$bicodon){
 }
 
 
+# Tasas individuales de cada codon
+datos_bicodones$tasaCodon1 <- numeric(length = 3721)
+datos_bicodones$tasaCodon2 <- numeric(length = 3721)
 
+for(i in datos_codones$codon){
+  for(j in datos_bicodones$primerCodon){
+  if(i == j){
+    datos_bicodones[datos_bicodones$primerCodon == i, 8] <- datos_codones[datos_codones$codon == i, 4]
+    }
+  }
+}
 
+for(j in datos_codones$codon){
+  for(k in datos_bicodones$segundoCodon){
+    if(j == k){
+      datos_bicodones[datos_bicodones$segundoCodon == j, 9] <- datos_codones[datos_codones$codon == j, 4]    
+    }  
+  }
+}
+
+# Cálculo del producto de tasas
+datos_bicodones$productoDeTasas <- datos_bicodones$tasaCodon1 * datos_bicodones$tasaCodon2
+
+# Determinación de pares de codones inhibitorios
+datos_bicodones$cod1Bool <- logical(length = 3721)
+datos_bicodones$cod2Bool <- logical(length = 3721)
+
+for(i in codones_raros){
+  for(j in datos_bicodones$primerCodon){
+    if(i == j){
+      datos_bicodones[datos_bicodones$primerCodon == i, 11] <- dflogic[1, 2]
+    }
+  }
+}
+
+for(i in codones_raros){
+  for(k in datos_bicodones$segundoCodon){
+    if(i == k){
+      datos_bicodones[datos_bicodones$segundoCodon == i, 12] <- dflogic[1, 2]
+    }
+  }
+}
+
+datos_bicodones$esRaro <- datos_bicodones$cod1Bool * datos_bicodones$cod2Bool # Raro: 1, no raro: 0
 
 
 
