57e20fa6fceae9ebaec7d2a669a0c4f392589229
Facundo Martínez <90737264+fx-biocoder@users.noreply.github.com>
2022-01-27 13:54:02 -0300

    Avances
    Añadí tres columnas al dataframe del análisis de bicodones, para identificar el dipéptido generado y realizar posteriormente un Z-score distribution plot. También hice algunas adiciones menores, como un plot de pares inhibitorios conservados.

--- dataframes/analisis.r
+++ dataframes/analisis.r
@@ -29,7 +29,11 @@ codones_barplot <- ggplot(datos_codones,
                           aes(   x = codon,
                                  y = tasaConservacion,
                               fill = esRaro)) +
-                   geom_bar(stat = "identity")
+                   geom_bar(stat = "identity") +
+                   labs(    x = "Codón", 
+                            y = "Tasa de conservación",
+                        title = "Conservación de codones individuales") +
+                   guides(fill=guide_legend(title="Codón raro:"))
 
 codones_barplot
 
@@ -89,18 +93,12 @@ for(i in codones_raros){
 for(i in codones_raros){
   for(k in datos_bicodones$segundoCodon){
     if(i == k){
-      datos_bicodones[datos_bicodones$segundoCodon == K, 12] <- TRUE
+      datos_bicodones[datos_bicodones$segundoCodon == k, 12] <- TRUE
     }
   }
 }
 
 datos_bicodones$esRaro <- datos_bicodones$cod1Bool * datos_bicodones$cod2Bool # Raro: 1, no raro: 0
-
-# Ajuste lineal
-modelo_lineal <- lm(tasaConservacion ~ 0 + productoDeTasas, data = datos_bicodones)
-
-summary(modelo_lineal)
-plot(modelo_lineal) # No hay normalidad. Pero ajusta con un r cuadrado de 0.81
 
 modelo <- ggplot(data = datos_bicodones,
                  aes(   x = productoDeTasas,
@@ -114,41 +112,88 @@ bicodones_raros <- subset(datos_bicodones, esRaro == 1)
 
 plot_pares_inhibitorios <- ggplot(data = bicodones_raros,
                                   aes(   x = productoDeTasas,
-                                         y = tasaConservacion)) +
-                          geom_rect(aes( xmin = 0, xmax = 0.05, 
-                                          ymin = 0, ymax = 0.05)) +
+                                         y = tasaConservacion,
+                                     label = bicodon)) +
+                          geom_rect(aes(xmin = 0, xmax = 0.05, 
+                                        ymin = 0, ymax = 0.05)) +
                            geom_point(color = "blueviolet") +
-                           xlim(0, 0.20) +
+                           xlim(0, 0.05) +
                            ylim(0, 0.2) +
                            geom_hline(yintercept = 0.05, linetype = "dotdash") +
                            geom_vline(xintercept = 0.05, linetype = "dotdash") +
                            geom_abline(aes(intercept = 0,
                                                slope = 1)) +
                            labs(x = "Producto de tasas de conservación de codones constituyentes",
-                                y = "Tasa de conservación real del bicodón raro") 
+                                y = "Tasa de conservación real del bicodón raro",
+                                title = "Tasa real vs. tasa predicha para bicodones")  +
+                           geom_text(aes(label = bicodon), hjust = 0, vjust = 0)
                            
 plot_pares_inhibitorios
 
 # Seleccionar aquellos codones cuya tasa de conservación supera la predicha
 datos_bicodones$tasaRealSobrePredicha <- datos_bicodones$tasaConservacion / datos_bicodones$productoDeTasas
 bicodones_raros <- subset(datos_bicodones, esRaro == 1)
-bicodones_raros_conservados <- subset(bicodones_raros, tasaRealSobrePredicha > 50)
+bicodones_raros_conservados <- subset(bicodones_raros, tasaRealSobrePredicha > 25)
+
+plot_pares_inhibitorios_conservados <- ggplot(data = bicodones_raros_conservados,
+                                              aes(   x = productoDeTasas,
+                                                     y = tasaConservacion,
+                                                     label = bicodon)) +
+                                       geom_point(color = "blueviolet") +
+                                       xlim(0, 0.004) +
+                                       ylim(0, 0.2) +
+                                       labs(x = "Producto de tasas de conservación de codones constituyentes",
+                                            y = "Tasa de conservación real del bicodón raro",
+                                            title = "Pares de codones conservados")  +
+                                       geom_text(aes(label = bicodon), hjust = 0, vjust = 0)
+
+plot_pares_inhibitorios_conservados
 
 # Ver qué sucede con los bicodones normales
 bicodones_normales <- subset(datos_bicodones, esRaro == 0)
 bicodones_normales_conservados <- subset(bicodones_normales, tasaRealSobrePredicha > 50)
 
-# write.csv(datos_codones, file = "/home/usuario/Documentos/GitHub/Conservacion-de-codones-raros/dataframes/analisis_codones.csv", row.names = FALSE)
-# write.csv(datos_bicodones, file = "/home/usuario/Documentos/GitHub/Conservacion-de-codones-raros/dataframes/analisis_bicodones.csv", row.names = FALSE)
+# Grabar dataframes
+write.csv(datos_codones, file = "/home/usuario/Documentos/GitHub/Conservacion-de-codones-raros/dataframes/analisis_codones.csv", row.names = FALSE)
+write.csv(datos_bicodones, file = "/home/usuario/Documentos/GitHub/Conservacion-de-codones-raros/dataframes/analisis_bicodones.csv", row.names = FALSE)
 
+# Ajuste lineal
+modelo_lineal <- lm(tasaConservacion ~ 0 + productoDeTasas, data = datos_bicodones)
 
+summary(modelo_lineal)
+plot(modelo_lineal) # No hay normalidad. Pero ajusta con un r cuadrado de 0.81
 
+# Z-score distribution plots
+# Necesito agrupar pares de codones de acuerdo al dipéptido que codifican
+codon   <- c("TTT", "TTC", "TTA", "TTG", "CTT", "CTC", "CTA", "CTG", "ATT", "ATC", "ATA", "ATG", "GTT", "GTC", "GTA", "GTG",
+             "TCT", "TCC", "TCA", "TCG", "CCT", "CCC", "CCA", "CCG", "ACT", "ACC", "ACA", "ACG", "GCT", "GCC", "GCA", "GCG",
+             "TAT", "TAC", "CAT", "CAC", "CAA", "CAG", "AAT", "AAC", "AAA", "AAG", "GAT", "GAC", "GAA", "GAG",
+             "TGT", "TGC", "TGG", "CGT", "CGC", "CGA", "CGG", "AGT", "AGC", "AGA", "AGG", "GGT", "GGC", "GGA", "GGG")
 
+peptido <- c("Phe", "Phe", "Leu", "Leu", "Leu", "Leu", "Leu", "Leu", "Ile", "Ile", "Ile", "Met", "Val", "Val", "Val", "Val",
+             "Ser", "Ser", "Ser", "Ser", "Pro", "Pro", "Pro", "Pro", "Thr", "Thr", "Thr", "Thr", "Ala", "Ala", "Ala", "Ala",
+             "Tyr", "Tyr", "His", "His", "Gln", "Gln", "Asn", "Asn", "Lys", "Lys", "Asp", "Asp", "Glu", "Glu",
+             "Cys", "Cys", "Trp", "Arg", "Arg", "Arg", "Arg", "Ser", "Ser", "Arg", "Arg", "Gly", "Gly", "Gly", "Gly")
 
+peptido_segun_codon <- data.frame(codon, peptido, row.names = NULL)
+datos_bicodones$primerPeptido <- character(length = 3721)
+datos_bicodones$segundoPeptido <- character(length = 3721)
 
+for(i in peptido_segun_codon$codon){
+  for(j in datos_bicodones$primerCodon){
+    if(i == j){
+      datos_bicodones[datos_bicodones$primerCodon == j, 14] <- peptido_segun_codon[peptido_segun_codon$codon == j, 2]
+    }
+  }
+}
 
+for(i in peptido_segun_codon$codon){
+  for(k in datos_bicodones$segundoCodon){
+    if(i == k){
+      datos_bicodones[datos_bicodones$segundoCodon == k, 15] <- peptido_segun_codon[peptido_segun_codon$codon == k, 2]
+    }
+  }
+}
 
-
-
-
+datos_bicodones$dipeptido <- paste(datos_bicodones$primerPeptido, datos_bicodones$segundoPeptido, sep = "-")
 
