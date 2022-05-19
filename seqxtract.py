97d8dd238516f70917a4ba2d7fd1843b6ca4efb6
Facundo Martínez <90737264+fx-biocoder@users.noreply.github.com>
2022-05-19 10:10:44 -0300

    Excepción "extensión inválida"

--- seqxtract.py
+++ seqxtract.py
@@ -2,18 +2,23 @@
 #           Esta función toma como argumento un archivo de la lista creada con lista_de_archivos()
 #           para abrirlo y hacer un sorting de las líneas presentes en el mismo
 def extraer_secuencias(archivo):
+    # Chequeo de extensión
+    if archivo.endswith('.afa') == False:
+        raise Exception('Error: extensión de archivo inválida, debe tener extensión ".afa". ¿Has modificado el script "listfiles.py"?')
+    
+    # Importaciones y elementos necesarios
     import os
     textlist, arrX, arrY = [], [], []
     auxvar = True
 
     #   Almacenamiento de cada línea del archivo en "textlist"
-    print("Extrayendo alineamientos del archivo " + archivo)
+    print(f"Extrayendo alineamientos del archivo {archivo}")
     archivo_abierto = open(os.path.expanduser(archivo), "r")
     for line in archivo_abierto:
         stripped_line = line.strip()
         line_list = stripped_line.split()
         textlist.append(line_list)
-    print("Extracción exitosa a partir del archivo " + archivo)
+    print(f"Extracción exitosa a partir del archivo {archivo}")
 
     #   Sorting de las líneas mediante el booleano auxvar
     #   Este booleano hace las veces de switch, y envía las líneas a una lista u otra
@@ -31,6 +36,6 @@ def extraer_secuencias(archivo):
     arrXY = []
     for i in range(len(arrX)):
         arrXY.append([arrX[i], arrY[i]])
-    print("La creación del array de alineamiento ha sido exitosa para el archivo " + archivo)
+    print(f"La creación del array de alineamiento ha sido exitosa para el archivo {archivo}")
     
     return arrXY
