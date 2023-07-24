e7ff311402ead2fcc8146aaaff34a6879dfa4606
Facundo Martínez <90737264+fx-biocoder@users.noreply.github.com>
2023-07-24 20:58:14 -0300

    Update intro.py

--- utils/intro.py
+++ utils/intro.py
@@ -6,10 +6,10 @@ def intro_message():
         int: Generic return value
     """
     print("BioSeeker v0.0.1")
-    print("Author: Facundo Martínez")
-    print("Official repository: https://github.com/fx-biocoder/BioSeeker")
+    print("Author: SouthernBio, Open Source Bioinformatics Collective from Argentina")
+    print("Official repository: https://github.com/SouthernBio/BioSeeker")
     print("License: GNU General Public License v3.0, see LICENSE")
-    print("Wiki: https://github.com/fx-biocoder/BioSeeker/wiki")
+    print("Wiki: https://github.com/SouthernBio/BioSeeker/wiki")
     print("--------------------------------")
     print("\n")
     input("Press any key to start the analysis on the current directory...")
